31af6abc115d9621aa06fbe43c42a6bea61ddeff
Namithaspanikar <namithaspanikar@gmail.com>
2024-02-11 23:25:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function Calculator(value) {
+    document.getElementById('display').value += value;
+}
+
+function Clear() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
